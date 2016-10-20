24ecf04be25d4e004ced23d7dd574562ca57c37b
Asif Altaf <asifaltaf13@gmail.com>
2016-10-20 23:22:30 +0200

    task_1_c added

--- Sheet_1_Basic_Control_Flow/main.cpp
+++ Sheet_1_Basic_Control_Flow/main.cpp
@@ -6,12 +6,14 @@
 //useful library functions for performing "console input and output" 
 #include <conio.h> 
 
+#include <string>
 
 //A namespace is a declarative region that provides a scope to the identifiers (the names of types, functions, variables, etc) inside it.
 //Namespaces are used to organize code into logical groups and to prevent name collisions that can occur especially when your
 //code base includes multiple libraries.
 using namespace std; 
 
+void task_1_c();
 
 //int is the return type of the function main
 //main is the name of the function ... this is where the program enters first
@@ -22,6 +24,31 @@ int main(int argc, char** argv)
 	
 	cout << "Hello world" << endl; // cout: standard character output stream ... << insertion operator ... "Hello World" is the content in quotes 
 
+	//task_1_c
+	task_1_c();
+
 	_getch(); // a function from conio header ... used to get character from the user
 	return 0; // return statement of the function
-} // ends the scope/body of the function
+} // ends the scope/body of the function
+
+void task_1_c()
+{
+	string name;
+	int day, month, year;
+	int cday = 29, cmonth = 4, cyear = 2016;
+
+	cout << "Enter your name: " << flush; // flush clears the buffer
+	getline(cin, name); // becuase extraction operator >> does not take spaces
+
+	cout << "Enter your day of birth: " << flush;
+	cin >> day;
+
+	cout << "Enter your month of birth: " << flush;
+	cin >> month;
+
+	cout << "Enter your year of birth: " << flush;
+	cin >> year;
+
+	cout << "Hi, " << name << ".\nYour age is:" << cyear - year << endl;
+
+}
