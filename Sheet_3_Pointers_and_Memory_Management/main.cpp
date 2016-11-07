cc08dc8cc4bca984efcc7553816dbcfe84dc149f
Asif Altaf <asifaltaf13@gmail.com>
2016-11-07 02:14:42 +0100

    assignment 3, question 2 added

--- Sheet_3_Pointers_and_Memory_Management/main.cpp
+++ Sheet_3_Pointers_and_Memory_Management/main.cpp
@@ -1,104 +1,106 @@
 #include <iostream>
+#include <math.h>
+
+using namespace std;
 
 
-void callByValue(int value) {
-    value = 5;
+int string_length(const char* string) {
+    int length = 0;
+
+    for(const char* ptr = string; *ptr != '\0'; ++ ptr) {
+        ++ length;
+    }
+    return length;
 }
-void callByReference(int& reference) {
-    reference = 10;
+
+
+char* string_copy(const char* string) {
+// we need to add 1 because of ’\0’
+    char* result = new char[string_length(string) + 1];
+
+    int i = 0;
+    for(; i < string_length(string) ; i++){
+
+        result[i] = string[i];
+
+    }
+
+    result[i] = '\0';
+
+// write your code here (remember zero-termination !)
+
+    return result;
 }
-void callByPointer(int* pointer) {
-    *pointer = 20;
+
+
+
+float mean(float* arr ,int size){
+
+    float sum = 0.f;
+
+    for(int i = 0 ; i < size ; i++){
+
+        sum += arr[i];
+
+    }
+
+    return sum/(float)size;
 }
 
-int main()
-{
+float variance(float* arr, int size){
 
-    double number = 10.0;
-    double number2= 11.0;
-    double& reference(number); // reference to variable number
-    double* pointer(&number); // pointer to variable number
-    std::cout << &number << "; " << &reference << "; " << pointer << std::endl;
-    std::cout << number << "; " << reference << "; " << *pointer << std::endl;
-    *pointer = 15;
-    std::cout << number << "; " << reference << "; " << *pointer << std::endl;
-    reference = 50;
-    std::cout << number << "; " << reference << "; " << *pointer << std::endl;
-    number = 10;
-    std::cout << number << "; " << reference << "; " << *pointer << std::endl;
+    float m = mean(arr, size);
 
-    reference = number2;
+    for(int i = 0 ; i < size ; i++){
 
-    std::cout << reference <<std::endl;
+        arr[i] = pow(arr[i]-m, 2);
 
-
-    int value = 0;
-    callByValue(value);
-    std::cout << value << std::endl;
-    callByReference(value);
-    std::cout << value << std::endl;
-    callByPointer(&value);
-    std::cout << value << std::endl;
-
-    std::cout << std::endl;
-
-    double array[5] = { 10.0, 20.0, 30.0, 40.0, 50.0 }; // array with five values is initialized
-    double* begin = array; // begin pointer starts pointing to head of the array
-
-    std::cout << *(begin + 0) << std::endl; // first item is printed
-    std::cout << *(begin + 1) << std::endl; // second item is printed
-    std::cout << *(begin + 2) << std::endl; // third item is printed
-
-    double* end = array + 5; // address where 6th item could have been is assigned to end pointer
-
-    int diff = end - begin; //  integer diff stores the difference of end and begin variable locations (which is 5)
-    diff /= 2; // 5/2 comes out to be 2
-
-    double value2 = *(begin + 1) + *(end - (diff)); // 20 + 40 = 60 ... 20 is second element, 40 comes if we subtract
-    // diff number of location from the sixth element's address
-
-    std::cout << value2 << std::endl; // 60 is printed
-    std::cout << *end << std::endl; // address of 6th element (which was initially not the part of the array and hence
-    // contains garbage value) is printed
-
-    std::cout << std::endl;
-    std::cout << std::endl;
-
-    double* ptr = begin;    // ptr pointer starts pointing to where begin points which is the start of the array
-    while (ptr != end) {    // this while loop runs until ptr starts pointing to where end pointer is pointing
-        std::cout << *(ptr ++) << " ";
-    }
-    std::cout << std::endl; // adding a new line
-    ptr = end;              // ptr points to the place where end points (which is one after the last element of array)
-    while (ptr != begin) {  // this while loop breaks when the ptr points to where begin is pointing
-        std::cout << *(-- ptr) << " ";  // this cout decreases the pointer value before cout runs
-    } ;
-    std::cout << std::endl; // adding a new line
-
-
-    std::cout << std::endl;
-    std::cout << std::endl;
-
-
-    for(const double* ptr=end-1; ptr != begin; )  // 
-    {
-        std::cout << *(-- ptr) << " ";
     }
 
-    std::cout << std::endl;
+    return mean(arr, size);
 
-    for(const double* ptr=end-1; ptr != begin; --ptr)
-    {
-        std::cout << *ptr << " ";
+}
+
+
+int main(int argc, char** argv) {
+
+    const char* string_c = "This is a string and is a long one so that we can create memory leaks when it is copied and not deleted";
+// write your code here
+
+    char * result = NULL;
+    result = string_copy(string_c);
+
+//    for(int i = 0 ; i < 1000000 ; i++){
+//
+//        char *temp = result;
+//        result = string_copy(result);
+//        delete [] temp;
+//
+//        // if we don't delete the temp strings, the program leaks a lot of memory
+//    }
+
+    cout<<result<<endl;
+    delete [] result;
+
+
+    int count;
+    float* arr;
+
+    cout<<"Please enter a count: "<<endl;
+    cin>>count;
+
+    arr = new float[count];
+    cout<<"Enter the floats and press enter: "<<endl;
+
+    for(int i = 0 ; i < count ; i++){
+        cin>>arr[i];
     }
 
-    std::cout << std::endl;
+    cout<<"Mean: "<<mean(arr, count)<<endl;
+    cout<<"variance: "<<variance(arr, count)<<endl;
 
-    for(const double* ptr=end-1; ptr >= begin; --ptr)
-    {
-        std::cout << *ptr << " ";
-    }
-
+    delete [] arr;
 
     return 0;
+
 }
